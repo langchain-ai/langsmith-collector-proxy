a2a50b0d2a18d3b5bf7e2929d1c70e3ac12057d8
angus-langchain <angus@langchain.dev>
2025-07-22 12:31:11 -0700

    feat: Refactor Otel converter to match LS backend (#4)

--- internal/util/util.go
+++ internal/util/util.go
@@ -5,6 +5,10 @@ import (
 	"time"
 )
 
+func StringPtr(s string) *string {
+	return &s
+}
+
 // create a dotted_order for a run
 func NewDottedOrder(id string) string {
 	s := time.Now().UTC().Format(time.RFC3339Nano)
